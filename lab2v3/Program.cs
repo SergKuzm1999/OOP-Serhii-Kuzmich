bb278e7fa36b6e00633c79d07c3f8c74c7ec7d51
SergKuzm1999 <sergiy.kuzmich1999@gmail.com>
2025-09-22 01:01:21 +0300

    Lab 3: Inheritance

--- lab2v3/Program.cs
+++ lab2v3/Program.cs
@@ -10,9 +10,8 @@
 
         Vector3D vect3 = vect1 + vect2;
         Vector3D vect4 = vect1 - vect2;
-        Console.WriteLine();
-        Console.WriteLine($"Vector 1 + Vector 2 = {vect3.X} {vect3.Y} {vect3.Z}" );
-        Console.WriteLine($"Vector 1 - Vector 2 = {vect4.X} {vect4.Y} {vect4.Z}" );
+        Console.WriteLine($"Vector 1 + Vector 2 = {vect3.X} {vect3.Y} {vect3.Z}");
+        Console.WriteLine($"Vector 1 - Vector 2 = {vect4.X} {vect4.Y} {vect4.Z}");
         Console.WriteLine();
         Console.WriteLine($"Vector 1 == Vector 2 : {vect1 == vect2}");
         Console.WriteLine($"Vector 1 != Vector 2 : {vect1 != vect2}");
@@ -20,103 +19,104 @@
         Console.WriteLine();
         Console.WriteLine($"Index Vector 1 [0] : {vect1[0]}");
         Console.ReadKey();
-    }
-}
 
-class Vector3D
-{
-    private double _x, _y, _z;
+    }
 
-    public double X
+    class Vector3D
     {
-        get { return _x; }
-        set
+        private double _x, _y, _z;
+
+        public double X
         {
-            _x = value;
-        }
-    }
-    public double Y
-    {
-        get { return _y; }
-        set
-        {
-            _y = value;
-        }
-    }
-    public double Z
-    {
-        get { return _z; }
-        set
-        {
-            _z = value;
-        }
-    }
-    public double Length
-    {
-        get
-        {
-            return Math.Sqrt(_x * _x + _y * _y + _z * _z);
-        }
-    }
-    public Vector3D(double x, double y, double z)
-    {
-        this._x = x;
-        this._y = y;
-        this._z = z;
-        ValidateLength();
-    }
-    private void ValidateLength()
-    {
-        if (Length > 100)
-            throw new ArgumentOutOfRangeException("Length", "Довжина вектора не може перевищувати 100");
-    }
-    public static Vector3D operator +(Vector3D a, Vector3D b)
-    {
-        return new Vector3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
-    }
-    public static Vector3D operator -(Vector3D a, Vector3D b)
-    {
-        return new Vector3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
-    }
-    public static bool operator ==(Vector3D a, Vector3D b)
-    {
-        return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
-    }
-    public static bool operator !=(Vector3D a, Vector3D b)
-    {
-        return !(a == b);
-    }
-    public double this[int index]
-    {
-        get
-        {
-            switch (index)
+            get { return _x; }
+            set
             {
-                case 0:
-                    return _x;
-                case 1:
-                    return _y;
-                case 2:
-                    return _z;
-                default:
-                    throw new IndexOutOfRangeException("Індекс має бути 0, 1 або 2");
+                _x = value;
             }
         }
-        set
+        public double Y
         {
-            switch (index)
+            get { return _y; }
+            set
             {
-                case 0:
-                    _x = value;
-                    break;
-                case 1:
-                    _y = value;
-                    break;
-                case 2:
-                    _z = value;
-                    break;
-                default:
-                    throw new IndexOutOfRangeException("Індекс має бути 0, 1 або 2");
+                _y = value;
+            }
+        }
+        public double Z
+        {
+            get { return _z; }
+            set
+            {
+                _z = value;
+            }
+        }
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(_x * _x + _y * _y + _z * _z);
+            }
+        }
+        public Vector3D(double x, double y, double z)
+        {
+            this._x = x;
+            this._y = y;
+            this._z = z;
+            ValidateLength();
+        }
+        private void ValidateLength()
+        {
+            if (Length > 100)
+                throw new ArgumentOutOfRangeException("Length", "Довжина вектора не може перевищувати 100");
+        }
+        public static Vector3D operator +(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+        public static Vector3D operator -(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+        public static bool operator ==(Vector3D a, Vector3D b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+        public static bool operator !=(Vector3D a, Vector3D b)
+        {
+            return !(a == b);
+        }
+        public double this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return _x;
+                    case 1:
+                        return _y;
+                    case 2:
+                        return _z;
+                    default:
+                        throw new IndexOutOfRangeException("Індекс має бути 0, 1 або 2");
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        _x = value;
+                        break;
+                    case 1:
+                        _y = value;
+                        break;
+                    case 2:
+                        _z = value;
+                        break;
+                    default:
+                        throw new IndexOutOfRangeException("Індекс має бути 0, 1 або 2");
+                }
             }
         }
     }
---
+++ lab3v3/Program.cs
@@ -0,0 +1,73 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<Animal> animals = new List<Animal>();
+        animals.Add(new Dog("Buddy", 25.0, 1.6));
+        animals.Add(new Cat("Whiskers", 5.25, 1.2));
+        animals.Add(new Dog("Max", 30.0, 2.0));
+        animals.Add(new Cat("Luna", 4.5, 1.4));
+        
+        foreach (Animal animal in animals)
+        {
+            Console.WriteLine($"{animal.Name} says: {animal.Speak()}");
+            Console.WriteLine($"{animal.Name} eat per day : {animal.Eat():F0} calories");
+            Console.WriteLine();
+        }
+        for (int i = 0; i < animals.Count; i++)
+        {
+            for (int j = i + 1; j < animals.Count; j++)
+            {
+                CompareFood(animals[i], animals[j]);
+            }
+        }
+    }
+    static void CompareFood(Animal a1, Animal a2)//порівняння потреби в їжі
+    {
+        double eat1 = a1.Eat();
+        double eat2 = a2.Eat();
+        if (eat1 > eat2)
+            Console.WriteLine($"{a1.Name} needs more food than {a2.Name}");
+        else if (eat1 < eat2)
+            Console.WriteLine($"{a2.Name} needs more food than {a1.Name}");
+        else
+            Console.WriteLine($"{a1.Name} and {a2.Name} need the same amount of food");
+    }
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double CoefficientActivity { get; set; } // коефіцієнт активності
+        public Animal(string name, double weight, double coefficient_activity)
+        {
+            Name = name;
+            Weight = weight;
+            CoefficientActivity = coefficient_activity;
+        }
+        public abstract string Speak();
+        public virtual double Eat()
+        {
+            double RER = 70 * Math.Pow(Weight, 0.75);  // базова потреба
+            double MER = RER * CoefficientActivity;   // добова норма з активністю
+            return MER;
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog(string name, double weight, double coefficient_activity) : base(name, weight, coefficient_activity)
+        {}
+        public override string Speak()
+        {
+            return "Woof!";
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat(string name, double weight, double coefficient_activity) : base(name, weight, coefficient_activity)
+        {}
+        public override string Speak()
+        {
+            return "Meow!";
+        }
+    }
+}
